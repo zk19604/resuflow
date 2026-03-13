82b490a9dafb350166bbe381986b2d5302b15c5d
Subaina Monib <subainamonib3@gmail.com>
2026-03-14 01:51:45 +0500

    Mongo db

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const uploadRoute = require('./routes/upload');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -12,4 +13,11 @@ app.use(express.json());
 app.use('/api/cv', uploadRoute);
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+}).catch((err) => {
+    console.log(err);
+    process.exit(1);
+});
